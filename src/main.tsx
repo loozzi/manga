055cc9d93c97a3f0e608f21dc0677d61764e006f
Nguyen Quang Thao <nquangthao19072004@gmail.com>
2024-01-18 20:15:17 +0700

    add vercel/anl

--- src/main.tsx
+++ src/main.tsx
@@ -3,12 +3,14 @@ import { unstable_HistoryRouter as HistoryRouter } from 'react-router-dom'
 import App from './App.tsx'
 import { history } from './configs/history.tsx'
 import './index.css'
+import { Analytics } from '@vercel/analytics/react'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   // <React.StrictMode>
   // ts-expect-error
   <HistoryRouter history={history}>
     <App />
+    <Analytics />
   </HistoryRouter>
   // </React.StrictMode>
 )
